3c8b921230de5311ff061c3c48e656f7fc549849
Mark Canlas <m4rkd0tc0de@gmail.com>
2022-08-12 14:43:30 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+const PageTransition = () => {
+  //Button click active class
+  sectBtn.forEach((section) => {
+    section.addEventListener("click", (event) => {
+      const button = event.target;
+      sectBtn.forEach((btn) => btn.classList.remove("active-btn"));
+      button.classList.add("active-btn");
+    });
+  });
+
+  //Section Active class
+  allSections.addEventListener("click", (event) => {
+    const id = event.target.dataset.id;
+    if (id) {
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      event.target.classList.add("active");
+
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+};
+
+PageTransition();
